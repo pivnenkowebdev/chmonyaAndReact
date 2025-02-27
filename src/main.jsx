745ed8eca4c079bc032ed027070e018722f6c2f4
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-02-27 12:50:33 +0400

    Updates

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ListCard from "./modal.jsx";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ListCard />
-  </StrictMode>
-)

056805e9fd4db311f6684defd3c625e718f7fed8
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-02-27 12:48:18 +0400

    init: init gh-pages branch

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,21 @@
 const imagesInfo = {
     "chmonya-regular": {
-        src: "/chmonya-regular.jpg",
+        src: "./chmonya-regular.jpg",
         title: "чмоня обыкновенный",
         description: "Классический чмоня, которого часто можно встретить на приборной панели автомобиля"
     },
     "chmonya-zubenko": {
-        src: "/chmonya-zubenko.jpg",
+        src: "./chmonya-zubenko.jpg",
         title: "чмоня зубенко",
         description: "Стильный чмоня затмил собой оригинальный мем"
     },
     "chmonya-yandex-delivery": {
-        src: "/chmonya-yandex-delivery.jpg",
+        src: "./chmonya-yandex-delivery.jpg",
         title: "чмоня доставщик",
         description: "Такому доставщику можно простить холодную шаурму"
     },
     "chmonya-vendetta": {
-        src: "/chmonya-vendetta.jpg",
+        src: "./chmonya-vendetta.jpg",
         title: "чмоня мафиози",
         description: "Чмоня объявил вендетту, но забыл, что у него лапки"
     },
